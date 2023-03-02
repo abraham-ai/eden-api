f57cf8b4b1a228de9dede0ca8bfd5a95021bdeaf
Gene Kogan <kogan.gene@gmail.com>
2023-03-02 14:03:12 -0800

    file upload limit to 10MB

--- src/plugins/multipartPlugin.ts
+++ src/plugins/multipartPlugin.ts
@@ -7,7 +7,7 @@ export const registerMultipart = async (fastify: FastifyInstance) => {
         fieldNameSize: 100, // Max field name size in bytes
         fieldSize: 100,     // Max field value size in bytes
         fields: 10,         // Max number of non-file fields
-        fileSize: 1000000,  // For multipart forms, the max file size in bytes
+        fileSize: 10000000,  // For multipart forms, the max file size in bytes
         files: 1,           // Max number of file fields
         headerPairs: 2000   // Max number of header key=>value pairs
       }
